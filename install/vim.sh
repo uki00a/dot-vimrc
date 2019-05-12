64281cea7ccc5327ee51982197cc9a268ec42a2e
uki00a <uki00a@gmail.com>
2019-05-13 00:26:38 +0900

    refactor: rename script_dir to dotfiles

--- install/vim.sh
+++ install/vim.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-script_dir=$(cd $(dirname $0); pwd)
-vimrc=$script_dir/.vimrc
-vimfiles=$script_dir/vim
+dotfiles=$(cd $(dirname $0); pwd)
+vimrc=$dotfiles/.vimrc
+vimfiles=$dotfiles/vim
 
 ln -s $vimrc $HOME/.vimrc
 ln -s $vimfiles $HOME/.vim
